298bb83bbffd985fe740f4e80151cb4dd6b7a529
Nguyễn Đình Vinh <vinh@gmail.com>
2025-04-17 15:24:39 +0000

    add java version 2

--- App.java
+++ App.java
@@ -3,13 +3,13 @@ import java.io.OutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
-public class App {
+public class App {i
     public static void main(String[] args) throws IOException {
         int port = 8888;
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", port), 0);
 
         server.createContext("/", exchange -> {
-            String response = "Hello from simple Java app!";
+            String response = "Hello from simple Java app version 2";
             exchange.sendResponseHeaders(200, response.length());
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(response.getBytes());
